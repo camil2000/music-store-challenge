fcf455e525e6d2b7e8cda70a29d80f2045647b17
camil2000 <mariacamillapra18@gmail.com>
2026-03-09 17:09:50 -0500

    Transaction commit

--- musicstore/model.py
+++ musicstore/model.py
@@ -1,3 +1,13 @@
 from datetime import datetime
 
 # TODO: Add code here
+
+class Transaction:
+
+    SELL = 1
+    SUPPLY = 2
+
+    def __init__(self, type: int, copies: int):
+        self.type = type
+        self.copies = copies
+        self.date = datetime.now()
