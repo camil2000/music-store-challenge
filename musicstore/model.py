e568d01b6b021efd26da8858d5e66cad780f7d18
camil2000 <mariacamillapra18@gmail.com>
2026-03-09 17:17:49 -0500

    metodos commit

--- musicstore/model.py
+++ musicstore/model.py
@@ -25,3 +25,39 @@ class Disc:
         self.purchase_price = purchase_price
         self.quantity = quantity
 
+        self.transactions: list[Transaction] = []
+        self.song_list: list[str] = []
+
+    def add_song(self, song: str):
+        self.song_list.append(song)
+
+    def sell(self, copies: int):
+
+        if copies > self.quantity:
+            return False
+
+        self.quantity -= copies
+        self.transactions.append(Transaction(Transaction.SELL, copies))
+
+        return True
+
+    def supply(self, copies: int):
+
+        self.quantity += copies
+        self.transactions.append(Transaction(Transaction.SUPPLY, copies))
+
+    def copies_sold(self) -> int:
+
+        total = 0
+
+        for transaction in self.transactions:
+            if transaction.type == Transaction.SELL:
+                total += transaction.copies
+
+        return total
+
+    def __str__(self):
+
+        songs = ", ".join(self.song_list)
+
+        return f"SID: {self.sid}\nTitle: {self.title}\nArtist: {self.artist}\nSong List: {songs}"
